abb747b2103f91ee50cf177de0b82edb8d6dbd5c
Messer4 <mikle0497@gmail.com>
2018-12-05 17:11:34 +0200

    Fix naming

--- service/user.go
+++ service/user.go
@@ -16,13 +16,13 @@ type User struct {
 	Password string `db:"usr_password"`
 }
 
-func CreateUser(writer http.ResponseWriter, router *http.Request) {
+func CreateUser(w http.ResponseWriter, router *http.Request) {
 	decoder := json.NewDecoder(router.Body)
 	var user User
 	err := decoder.Decode(&user)
 	if err != nil {
-		writer.WriteHeader(http.StatusUnprocessableEntity)
-		writer.Write([]byte("422 - Cannot read the request's body."))
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("422 - Cannot read the request's body."))
 		return
 	}
 
@@ -34,8 +34,8 @@ func CreateUser(writer http.ResponseWriter, router *http.Request) {
 	_,err = db.GetClientMysql().Insert(squirrel.Insert(UsersTable).SetMap(data))
 	if err != nil {
 		log.Println(err)
-		writer.WriteHeader(http.StatusInternalServerError)
-		writer.Write([]byte("500 - Cannot create user."))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Cannot create user."))
 		return
 	}
 	js, err := json.Marshal(map[string]interface{}{
